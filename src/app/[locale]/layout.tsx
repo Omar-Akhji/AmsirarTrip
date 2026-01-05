2dde3283f309adc271e65516de2bcb731a3d713b
Omar-Akhji <amsirare@gmail.com>
2026-01-05 01:15:09 +0100

    Add Google Tag Manager (GTM-K22ZHHN6)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -213,6 +213,16 @@ export default async function LocaleLayout({
         />
       </head>
       <body>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K22ZHHN6');
+          `}
+        </Script>
         <Script
           src="https://kit.fontawesome.com/b7031c5d36.js"
           crossOrigin="anonymous"
